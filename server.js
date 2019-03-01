30ef10968133a7118b4a888ecc725bb6a8ca504d
Vahe Minasyan (Red Door Collaborative LLC) <v-vamin@microsoft.com>
2019-03-01 03:02:32 -0800

    updates

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = server.listen(process.env.PORT ||3000);
+var PORT = process.env.PORT ||3000;
 
 // Initialize Express
 var app = express();
