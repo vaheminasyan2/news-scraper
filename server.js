43999e566abe1b7c8b5699d88e0e99a8d21f1a10
Vahe Minasyan (Red Door Collaborative LLC) <v-vamin@microsoft.com>
2019-03-01 03:00:38 -0800

    updated

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = server.listen(process.env.PORT ||3000);
 
 // Initialize Express
 var app = express();
@@ -31,11 +31,11 @@ app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
 
-mongodb://<dbuser>:<dbpassword>@ds255930.mlab.com:55930/heroku_p35tvwgw
+//mongodb://<dbuser>:<dbpassword>@ds255930.mlab.com:55930/heroku_p35tvwgw
 
 // Connect to the Mongo DB
-var MONGODB_URI = "mongodb://<dbuser>:<dbpassword>@ds255930.mlab.com:55930/heroku_p35tvwgw" || "mongodb://localhost/newsScraper";
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/newsScraper";
+mongoose.connect(MONGODB_URI);
 
 // Routes
 
